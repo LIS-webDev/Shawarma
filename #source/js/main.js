02710e8e9088dec26fafb10ad4ff77faeb4ed4c4
Lazarev Ivan <lis.webdevelop@gmail.com>
2021-01-14 22:25:37 +0500

    Add ibg func

--- #source/js/main.js
+++ #source/js/main.js
@@ -11,6 +11,18 @@ burger.addEventListener('click', () => {
 });
 
 
+function ibg(){
+
+    let ibg=document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+        }
+    }
+}
+
+ibg();
+
 
 // function scrollToAnchor() {
 //     let linkAchors = document.querySelectorAll('.header__link');
